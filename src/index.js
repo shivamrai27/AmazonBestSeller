f6db4a72c3d4f678bb98a7db63f55654435bb721
Shivam Rai <200shivamrai@gmail.com>
2023-09-12 21:39:51 +0530

    BestSeller

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function BookList() {
+    return (
+        <section>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+}
+
+const Book = () => {
+    return (
+        <article>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
+};
+
+const Image = () => <h2>image placeholder</h2>;
+const Title = () => {
+    return <h2>Book Title</h2>;
+};
+const Author = () => <h4>Author</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
