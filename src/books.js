6a8ced286ffef627034bb3ed8dca9bb79aeb03d7
Shivam Rai <200shivamrai@gmail.com>
2023-09-22 18:35:54 +0530

    AmazonBestSeller

---
+++ src/books.js
@@ -0,0 +1,27 @@
+export const books = [
+    {
+        author: 'Morgan Housel',
+        title: 'The Psychology Of Money',
+        img: "https://m.media-amazon.com/images/I/71g2ednj0JL._SY466_.jpg",
+        id: 1
+    },
+    {
+        author: 'James Clear',
+        title: 'Atomic Habits',
+        img: "https://m.media-amazon.com/images/I/91bYsX41DVL._SY466_.jpg",
+        id: 2
+    },
+    {
+        author: 'Sadguru',
+        title: 'KARMA: A YOGI GUIDE',
+        img: "https://m.media-amazon.com/images/I/91xafByFlLL._SY466_.jpg",
+        id: 3
+    },
+    {
+        author: 'Anand Ranganathan',
+        title: 'Hindus in Hindu',
+        img: "https://m.media-amazon.com/images/I/91EtFGJvG8L._SY466_.jpg",
+        id: 4
+    }
+
+];
--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-
-const books = [
-    {
-        author: 'Morgan Housel',
-        title: 'The Psychology Of Money',
-        img: "https://m.media-amazon.com/images/I/71g2ednj0JL._SY466_.jpg",
-        id: 1
-    },
-    {
-        author: 'James Clear',
-        title: 'Atomic Habits',
-        img: "https://m.media-amazon.com/images/I/91bYsX41DVL._SY466_.jpg",
-        id: 2
-    },
-    {
-        author: 'Sadguru',
-        title: 'KARMA: A YOGI GUIDE',
-        img: "https://m.media-amazon.com/images/I/91xafByFlLL._SY466_.jpg",
-        id: 3
-    },
-    {
-        author: 'Anand Ranganathan',
-        title: 'Hindus in Hindu',
-        img: "https://m.media-amazon.com/images/I/91EtFGJvG8L._SY466_.jpg",
-        id: 4
-    }
-
-]
-
+import { books } from './books'
+import Book from './book'
 const BookList = () => {
     return (
         <section className='booklist'>
@@ -39,19 +12,5 @@ const BookList = () => {
         </section>
     );
 };
-
-
-const Book = (props) => {
-    const { img, title, author, } = props;
-
-    return (
-        <article className='book'>
-            <img src={img} alt={title} />
-            <h2>{title}</h2>
-            <h4>{author}</h4>
-        </article>
-    );
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList />);
